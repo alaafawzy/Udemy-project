05283351360a131ed34edfa4e9c44de440aa7614
Alaa Fawzy <alaafawzy963@gmail.com>
2022-10-26 21:42:44 +0200

    Updates

---
+++ precache-manifest.23d4ee4b5258c04b4273baea49ff45a4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "02fb56b475fe4f7b004f",
+    "url": "/Udemy-project/static/css/main.83a905bf.chunk.css"
+  },
+  {
+    "revision": "02fb56b475fe4f7b004f",
+    "url": "/Udemy-project/static/js/main.02fb56b4.chunk.js"
+  },
+  {
+    "revision": "4bf245bff49bb0b79407",
+    "url": "/Udemy-project/static/js/1.4bf245bf.chunk.js"
+  },
+  {
+    "revision": "dd3d7a64f577369253f5",
+    "url": "/Udemy-project/static/js/2.dd3d7a64.chunk.js"
+  },
+  {
+    "revision": "86ac18dfcfbc71017548",
+    "url": "/Udemy-project/static/js/runtime~main.86ac18df.js"
+  },
+  {
+    "revision": "f5448c2773cbe133cf2507ec69f80965",
+    "url": "/Udemy-project/index.html"
+  }
+];
